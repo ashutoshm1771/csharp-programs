3029af479f5af525bcade6c5b697dfc83621b214
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2021-02-09 16:51:24 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace C__Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basics of C# with Ashutosh Mishra");
+        }
+    }
+}
