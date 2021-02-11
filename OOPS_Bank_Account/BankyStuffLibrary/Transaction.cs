095c5905454c1bd61f6d9ff519956a94c382b8e6
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2021-02-11 20:19:06 +0530

    Add files via upload

---
+++ OOPS_Bank_Account/BankyStuffLibrary/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankyStuffLibrary
+{
+    public class Transaction
+    {
+        public decimal amount { get;  }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        public Transaction(decimal amount,DateTime date, string note)
+        {
+            this.amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
