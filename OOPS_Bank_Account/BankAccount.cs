b4961cee8e0543fe17f2af265a487a20141c67e0
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2021-02-11 10:40:15 +0530

    OOP in C#

---
+++ OOPS_Bank_Account/BankAccount.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { 
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.amount;
+                }
+                return balance;
+            }
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+        public BankAccount(string Name,decimal initialBalance)
+        {
+            this.Owner = Name;
+            MakeDeposit(initialBalance,DateTime.Now,"Initial Balance");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        } 
+        public void MakeDeposit(decimal amount,DateTime date,string note)
+        {
+            if ( amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), " Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if( Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal"); 
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+            // Header of the Table
+            report.AppendLine("Date\t\tAmount   \tNote");
+            foreach(var item in allTransactions)
+            {
+                // Rows of the Table
+                report.Append($"{item.Date.ToShortDateString()}\t{item.amount}\t{item.Notes}\n");
+            }
+            return report.ToString();
+        }
+    }
+}
---
+++ OOPS_Bank_Account/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
+            var account = new BankAccount(a, 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+
+            account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+
+            //try
+            //{
+            //    var invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(e.ToString());
+            //}
+
+            // Below Line generates exception
+            // account.MakeDeposit(-300, DateTime.Now, "Stealing");
+        }
+    }
+}
